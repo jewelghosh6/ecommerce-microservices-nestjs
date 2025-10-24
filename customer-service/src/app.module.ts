8573414ec872c41d177dc6ff5a5af73883dc396c
Jewel Ghosh <jewel.ghosh@vyrazu.com>
2025-10-24 19:11:25 +0530

    add some features

--- customer-service/src/app.module.ts
+++ customer-service/src/app.module.ts
@@ -1,9 +1,20 @@
+import { SequelizeModule } from '@nestjs/sequelize';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.DB_HOST,
+      port: +(process.env.DB_PORT || 5432),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      models: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
